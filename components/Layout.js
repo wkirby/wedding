7fd56e634d8ddb199bcd9fcbe8f946a009654da3
Wyatt Kirby <wyatt@apsis.io>
2018-12-12 15:13:01 -0800

    Add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,12 @@ import "../assets/styles/main.scss";
 export const Layout = ({ children, ...props }) => (
   <main id="main" {...props}>
     <Head>
+      <link
+        rel="shortcut icon"
+        href="/static/img/favicon.ico"
+        type="image/x-icon"
+      />
+      <link rel="icon" href="/static/img/favicon.ico" type="image/x-icon" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
 
