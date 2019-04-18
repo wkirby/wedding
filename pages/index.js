7ba738469191d5d9f6843326b20ccc75354ae859
Wyatt Kirby <wyatt@apsis.io>
2019-04-18 00:12:19 -0700

    Update stack image layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Portal } from "react-portal";
 import { Layout } from "../components/Layout";
-import StackGrid from "react-stack-grid";
+import StackGrid, { transitions, easings } from "react-stack-grid";
 import {
   Container,
   Modal,
@@ -29,11 +29,26 @@ const StackImage = ({ src }) => {
   );
 };
 
+const transition = transitions.scaleDown;
+
 export default () => {
   return (
     <Layout>
       <Container>
-        <StackGrid monitorImagesLoaded columnWidth={150}>
+        <StackGrid
+          monitorImagesLoaded
+          columnWidth={200}
+          duration={600}
+          gutterWidth={10}
+          gutterHeight={10}
+          easing={easings.cubicOut}
+          appearDelay={60}
+          appear={transition.appear}
+          appeared={transition.appeared}
+          enter={transition.enter}
+          entered={transition.entered}
+          leaved={transition.leaved}
+        >
           <StackImage src="https://picsum.photos/200/300/?random" />
           <StackImage src="https://picsum.photos/400/600/?random" />
           <StackImage src="https://picsum.photos/200/400/?random" />
