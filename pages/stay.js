31f37a7c02738219becaae7b68bd57bbd96fea1a
Wyatt Kirby <wyatt@apsis.io>
2019-04-27 19:12:08 -0700

    Update styles for stay page

--- pages/stay.js
+++ pages/stay.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { Nav, NavItem, NavLink, Row, Col, Container } from "reactstrap";
+import { Col, Container, Nav, NavItem, NavLink, Row } from "reactstrap";
 import { Layout } from "../components/Layout";
-import { PageHeader } from "../components/PageHeader";
-import { Title } from "../components/Title";
 import { LocationBlock } from "../components/LocationBlock";
+import { Image } from "../components/Image";
+import { PageHeader } from "../components/PageHeader";
+import { Section } from "../components/Section";
+import { SectionHeader } from "../components/SectionHeader";
 import hotelData from "../data/hotels.yaml";
 import rentalData from "../data/rentals.yaml";
 import travelData from "../data/travel.yaml";
 
+const NavItemHeader = ({ children, ...props }) => {
+  return (
+    <NavItem className="my-2 text-muted" {...props}>
+      <strong>{children}</strong>
+    </NavItem>
+  );
+};
+
 const StaySidebarMenu = props => (
-  <aside>
+  <aside className="sticky-top mb-3 d-none d-md-block">
     <Nav vertical>
-      <NavItem>Getting to Maine</NavItem>
+      <NavItemHeader>Getting to Maine</NavItemHeader>
+
       {travelData.map((t, i) => {
         return (
           <NavItem key={i}>
@@ -20,15 +31,16 @@ const StaySidebarMenu = props => (
         );
       })}
 
-      <NavItem>Vacation Rentals</NavItem>
-      <NavItem>
-        <NavLink href={`#airbnb`}>Airbnb</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink href={`#vrbo`}>VRBO</NavLink>
-      </NavItem>
+      <NavItemHeader>Vacation Rentals</NavItemHeader>
+      {rentalData.map((t, i) => {
+        return (
+          <NavItem key={i}>
+            <NavLink href={`#rental-${i}`}>{t.label}</NavLink>
+          </NavItem>
+        );
+      })}
 
-      <NavItem>Hotels</NavItem>
+      <NavItemHeader>Hotels</NavItemHeader>
       {hotelData.map((t, i) => {
         return (
           <NavItem key={i}>
@@ -45,14 +57,18 @@ export default () => {
     <Layout>
       <PageHeader title="Where to Stay" />
 
+      <div className="my-5">
+        <img src="static/img/hero/maine.png" className="img-fluid" />
+      </div>
+
       <Container>
         <Row>
-          <Col sm={4}>
+          <Col md={4}>
             <StaySidebarMenu />
           </Col>
 
           <Col>
-            <p>
+            <p className="lead">
               Maine’s midcoast region is a beautiful destination for a summer
               vacation. Because we have two venues, there are a wide range of
               places you could stay for your visit — depending on your
@@ -61,46 +77,68 @@ export default () => {
               encourage you to explore the region while you’re here.
             </p>
 
+            <Section>
+              <SectionHeader
+                title="Getting to Maine"
+                className="text-md-center"
+              />
+              <p>
+                Maine is a small state, and there are a number of ways to get
+                here. The main airport is PWM, located in Portland, ME --- but
+                it can be a pricey destination, especially in summer.
+              </p>
+              <p>
+                The next best airport is Boston Logan, in Boston, MA. From
+                there, you can rent a car and take I-95 north to Portland. The
+                drive is pleasant, and takes just about 2 hours.
+              </p>
+              <p>
+                If renting a car is not your thing, Concord Coach Lines runs
+                multiple buses every day from Boston Logan to the Portland
+                Transportation Center.
+              </p>
+
+              <div className="my-3">
+                {travelData.map((t, i) => (
+                  <LocationBlock id={`travel-${i}`} {...t} key={i} />
+                ))}
+              </div>
+            </Section>
+
             <hr />
 
-            <h3>Getting to Maine</h3>
-            <p>
-              Maine is a small state, and there are a number of ways to get
-              here. The main airport is PWM, located in Portland, ME --- but it
-              can be a pricey destination, especially in summer.
-            </p>
-            <p>
-              The next best airport is Boston Logan, in Boston, MA. From there,
-              you can rent a car and take I-95 north to Portland. The drive is
-              pleasant, and takes just about 2 hours.
-            </p>
-            <p>
-              If renting a car is not your thing, Concord Coach Lines runs
-              multiple buses every day from Boston Logan to the Portland
-              Transportation Center.
-            </p>
-
-            {travelData.map((t, i) => (
-              <LocationBlock {...t} key={i} />
-            ))}
+            <Section>
+              <SectionHeader
+                title="Vacation Rentals"
+                className="text-md-center"
+              />
+              <p>
+                Our preferred accomodations when visiting Maine, you can find
+                many beautiful homes to stay in on a number of vacation rental
+                sites. Below are our recommendations for sites where you might
+                find pleasant accomodations.
+              </p>
+              <div className="my-3">
+                {rentalData.map((t, i) => (
+                  <LocationBlock id={`rental-${i}`} {...t} key={i} />
+                ))}
+              </div>
+            </Section>
 
             <hr />
 
-            <h3>Vacation Rentals</h3>
-            <p>
-              Our preferred accomodations when visiting Maine, you can find many
-              beautiful homes to stay in on a number of vacation rental sites.
-              Below are our recommendations for sites where you might find
-              pleasant accomodations.
-            </p>
-            {rentalData.map((t, i) => (
-              <LocationBlock {...t} key={i} />
-            ))}
+            <Section>
+              <SectionHeader
+                title="Hotels &amp; Inns"
+                className="text-md-center"
+              />
 
-            <h3>Hotels &amp; Inns</h3>
-            {hotelData.map((t, i) => (
-              <LocationBlock {...t} key={i} />
-            ))}
+              <div className="my-3">
+                {hotelData.map((t, i) => (
+                  <LocationBlock id={`hotel-${i}`} {...t} key={i} />
+                ))}
+              </div>
+            </Section>
           </Col>
         </Row>
       </Container>
